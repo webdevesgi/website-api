f6109e56d0d0fc2c5ec432c30c4827230cca4889
Rémy Hannequin <remy.hannequin@gmail.com>
2013-11-11 23:09:53 +0100

    Add CORS to all requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+  before_action :allow_cors
+
+  def allow_cors
+    headers["Access-Control-Allow-Origin"] = "*"
+    headers["Access-Control-Allow-Methods"] = %w{GET POST PUT DELETE}.join(",")
+    headers["Access-Control-Allow-Headers"] =
+      %w{Origin Accept Content-Type X-Requested-With X-CSRF-Token}.join(",")
+  end
 end
