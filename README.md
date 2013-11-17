70b61d73db0ecd28194690ed9e4089162dafa15a
Rémy Hannequin <remy.hannequin@gmail.com>
2013-11-17 18:53:56 +0100

    Remove client app authorization

--- README.md
+++ README.md
@@ -17,13 +17,3 @@
 
     rake db:migrate db:test:prepare
     rspec spec
-
-Make sure you have created the file `config/application.yml` with the following content:
-
-    # Add application configuration variables here, as shown below.
-
-    AUTHORIZED_APPS:
-      - 127.0.0.1
-      - localhost
-
-It will enable your client application in development mode to access to the API.
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,10 @@
 class ApplicationController < ActionController::API
   before_action :allow_cors
-  before_action :is_an_authenticated_app?
 
   def allow_cors
     headers["Access-Control-Allow-Origin"] = "*"
     headers["Access-Control-Allow-Methods"] = %w{GET POST PUT DELETE}.join(",")
     headers["Access-Control-Allow-Headers"] =
       %w{Origin Accept Content-Type X-Requested-With X-CSRF-Token}.join(",")
-  end
-
-  def is_an_authenticated_app?
-    puts request.remote_host
-    authorized_apps = ENV['AUTHORIZED_APPS'].split(' ')
-    can_access = authorized_apps.include? request.remote_host
-    unless can_access
-      render json: {error: 'This is not an authenticated app'}, status: 403
-    end
   end
 end
